b1763f43da70b9ccd34d8a1ad6b2809cfc1530e3
malcolmsharpe <sharpe.malcolm@gmail.com>
2014-12-25 23:41:54 -0800

    Basic board display and game representation.

--- CatchupAI/MainWindow.xaml.cs
+++ CatchupAI/MainWindow.xaml.cs
@@ -20,9 +20,116 @@ namespace CatchupAI
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int Q = 50;
+        const int leftPad = Q;
+        const int topPad = Q;
+        Rectangle[,] hexes;
+        Game.Stone[,] prevDisplay;
+        Game game;
+
+        Brush brushBorder = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        Brush brushEmpty = new SolidColorBrush(Color.FromRgb(150, 150, 255));
+        Brush brushP1 = new SolidColorBrush(Color.FromRgb(50, 50, 255));
+        Brush brushP2 = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
         public MainWindow()
         {
             InitializeComponent();
+
+            hexes = new Rectangle[2 * Game.S - 1, 2 * Game.S - 1];
+            prevDisplay = new Game.Stone[2 * Game.S - 1, 2 * Game.S - 1];
+            for (int x = 0; x <= 2 * Game.S - 2; ++x)
+            {
+                for (int y = 0; y <= 2 * Game.S - 2; ++y)
+                {
+                    if (!Game.inBounds(x, y)) continue;
+                    hexes[x, y] = putHex(x, y);
+                }
+            }
+
+            newGame();
+        }
+
+        private void newGame()
+        {
+            game = new Game();
+            display();
+        }
+
+        private Rectangle putHex(int x, int y)
+        {
+            var rect = new System.Windows.Shapes.Rectangle();
+            rect.Stroke = brushBorder;
+            rect.Fill = brushEmpty;
+            rect.HorizontalAlignment = HorizontalAlignment.Left;
+            rect.VerticalAlignment = VerticalAlignment.Top;
+            rect.Height = rect.Width = Q;
+            rect.Margin = new Thickness(
+                leftPad + Q * (x + 0.5 * (Game.S - 1 - y)), // left
+                topPad + Q * y, // top
+                0, // right
+                0); // bottom
+            rect.Tag = encodePos(x, y);
+            rect.MouseUp += new MouseButtonEventHandler(this.hex_MouseUp);
+
+            Grid.SetRow(rect, 1);
+            mainGrid.Children.Add(rect);
+
+            return rect;
+        }
+
+        private object encodePos(int x, int y)
+        {
+            return y * (2 * Game.S - 1) + x;
+        }
+
+        private Tuple<int, int> decodePos(object tag)
+        {
+            int t = (int)tag;
+            int x = t % (2 * Game.S - 1);
+            int y = t / (2 * Game.S - 1);
+            return Tuple.Create(x, y);
+        }
+
+        private void display()
+        {
+            for (int x = 0; x <= 2 * Game.S - 2; ++x)
+            {
+                for (int y = 0; y <= 2 * Game.S - 2; ++y)
+                {
+                    if (!Game.inBounds(x, y)) continue;
+                    var stone = game.query(x, y);
+                    if (prevDisplay[x, y] != stone)
+                    {
+                        prevDisplay[x, y] = stone;
+
+                        switch (stone)
+                        {
+                            case Game.Stone.Empty:
+                                hexes[x, y].Fill = brushEmpty;
+                                break;
+                            case Game.Stone.Black:
+                                hexes[x, y].Fill = brushP1;
+                                break;
+                            case Game.Stone.White:
+                                hexes[x, y].Fill = brushP2;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void newGameMenu_Click(object sender, RoutedEventArgs e)
+        {
+            newGame();
+        }
+
+        private void hex_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var pos = decodePos(((Rectangle)sender).Tag);
+            game.userPlay(pos.Item1, pos.Item2);
+            display();
         }
     }
 }
